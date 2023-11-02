479215be5a36c7ac01860f1de4f626355b3dad23
Brian Richie Sr <brianrichiesr@gmail.com>
2023-11-01 21:25:40 -0700

    complete lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,19 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarf_list):
+    for i in range(0, len(dwarf_list)):
+        print(f"{i + 1}. {dwarf_list[i]}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteer_calls):
+    return [call.title() + "!" for call in planeteer_calls]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(word_list):
+    for word in word_list:
+        if len(word) > 4:
+            return True
+    
+    return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(word_list):
+    cheese_list = ["cheddar", "gouda", "camembert"]
+    for word in word_list:
+        if word in cheese_list:
+            return word
